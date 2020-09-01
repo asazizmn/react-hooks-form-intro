6980d4f58657d53cad3e85a85804b5fee2140def
S. Aziz <azizecomm@gmail.com>
2020-09-02 02:02:52 +0800

    Add register form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import React from 'react';
-import './App.css';
+import './index.css';
 
-function App() {
+
+/**
+ * Main component that comprises of the React Form
+ */
+const App = () => {
   return (
-    <div className="App">
-      Hello, React!
+    <div class="form-container">
+      <form class="register-form">
+        <TextInput
+          id="first-name"
+          name="firstName"
+          placeholder="First Name"
+          tooltip="Enter your first name"
+        />
+
+        <TextInput
+          id="last-name"
+          name="lastName"
+          placeholder="Last Name"
+          tooltip="Enter your last name"
+        />
+
+        <TextInput
+          id="email"
+          name="email"
+          placeholder="Email"
+          tooltip="Enter your email address"
+        />
+
+        <button class="form-field" type="submit" title="Submit registration form">
+          Register
+        </button>
+
+      </form>
     </div>
   );
-}
+};
+
+
+
+const TextInput = props => {
+  return (
+    <input
+      type="text"
+      class="form-field"
+      id={props.id}
+      name={props.name}
+      placeholder={props.placeholder}
+      title={props.tooltip}
+    />
+  );
+};
 
 export default App;
