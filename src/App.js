572638440ed0b20e9b0b469dd7f8112003427e52
S. Aziz <azizecomm@gmail.com>
2020-10-20 12:46:48 +0800

    update comment in App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import './index.css';
 const App = () => {
 
   // `useState(initialValue)` takes in an argument to set the initial state
-  // ... it then returns a reference to the state and 
+  // ... it then returns a reference to the state 
   // ... and function to allow changing the state at a later stage 
 
   // manage state of form fields
